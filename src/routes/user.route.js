8cc82be33fd8d5a856c963c6c461f74e20c5c715
sainathkamble <kamblesainath41@gmail.com>
2025-05-27 19:58:44 +0530

    Tried some other solution

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Router } from "express";
 import { 
     loginUser, 
@@ -35,50 +34,3 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-
-
-=======
-import { Router } from "express";
-import { 
-    loginUser, 
-    logoutUser, 
-    registerUser, 
-    refreshAccessToken, 
-    changeCurrentPassword, 
-    getCurrentUser, 
-    updateUserAvatar, 
-    updateAccountDetails,
-} from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-
-
-const router = Router()
-
-router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    )
-
-router.route("/login").post(loginUser)
-
-//secured routes
-router.route("/logout").post(verifyJWT,  logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-
-
->>>>>>> 749b53ae01bd7206b86f37b25ce57d09e6f79bda
-export default router
