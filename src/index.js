87e1cd6635274c2092d8566d44d944fea6e163de
sainathkamble <kamblesainath41@gmail.com>
2025-05-27 14:41:26 +0530

    s

--- src/index.js
+++ src/index.js
@@ -17,21 +17,3 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
 })
-=======
-import dotenv from 'dotenv';
-dotenv.config({
-    path: './.env'
-})
-import connectDB from "./db/index.js";
-import {app} from './app.js'
-
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
->>>>>>> 749b53ae01bd7206b86f37b25ce57d09e6f79bda
