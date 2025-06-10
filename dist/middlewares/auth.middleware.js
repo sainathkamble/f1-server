92e792896453b0c07ef35c744dcdacfcd8c73cd3
sainathkamble <kamblesainath41@gmail.com>
2025-06-10 21:32:03 +0530

    Build for production

--- dist/middlewares/auth.middleware.js
+++ dist/middlewares/auth.middleware.js
@@ -6,19 +6,19 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try{
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
+        
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
-    
+        console.log(token);
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    
+        console.log(decodedToken);
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if (!user) {
             throw new ApiError(401, "Invalid Access Token")
         }
-    
+        console.log(user);
         req.user = user;
         next();
     }catch(error){
