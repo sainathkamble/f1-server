223595b1abc3fa5fc197b5e9424332114144f2ef
sainathkamble <kamblesainath41@gmail.com>
2025-06-10 21:30:45 +0530

    Added secured requests

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -111,7 +111,8 @@ const loginUser = asyncHandler(async (req, res) =>{
 
     const options = {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production'
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'Strict'
     }
 
     return res
