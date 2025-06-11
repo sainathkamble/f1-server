bd3287513dda4bcb808113155046dac17ab33ac2
sainathkamble <kamblesainath41@gmail.com>
2025-06-11 14:22:10 +0530

    Build for deploy

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -10,15 +10,15 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
-        console.log(token);
+
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        console.log(decodedToken);
+        
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
     
         if (!user) {
             throw new ApiError(401, "Invalid Access Token")
         }
-        console.log(user);
+
         req.user = user;
         next();
     }catch(error){
