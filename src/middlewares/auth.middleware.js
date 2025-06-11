c557e7f3c75de1d23b5e78bf4b7cdb421c4d260e
sainathkamble <kamblesainath41@gmail.com>
2025-06-11 14:31:39 +0530

    Build tp deploy

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
         
         if (!token) {
-            throw new ApiError(401, "Unauthorized request")
+            throw new ApiError(401, "Unauthorized request this")
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
